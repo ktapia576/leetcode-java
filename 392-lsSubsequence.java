c6768621aaef9c306dd1423dcf381a8dfca82c72
Kevin Tapia <ktapia576@hotmail.com>
2025-03-10 22:24:27 -0400

    Add better approach with two pointers

--- 392-lsSubsequence.java
+++ 392-lsSubsequence.java
@@ -9,27 +9,32 @@
 // subsequence exists
 
 // time: O(n)
-// space: O(1
+// space: O(1)
+
+// My second approach... Better than first and easier to read:
+
+// starts from 0 instead of reverse, names the two pointers, removes result variable since not needed
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        boolean result = false;
-
-        if(s.length() == 0) { return true; }   // no matter what if s.length() == 0, it will be true due to contraints that t will always be 0 or greater
+        // no matter what if s.length() == 0, it will be true due to contraints that t will always be 0 or greater
+        if(s.length() == 0) { return true; }  
         if(t.length() < s.length()) { return false; } // no matter what if you t is smaller than s, it wont have a subsequence
 
         // Find chars
-        int sIndex = s.length()-1;
-        for(int i = t.length()-1; i >= 0; i--){
-            char currChar = t.charAt(i);
-            if(s.charAt(sIndex) == currChar) { 
-                if(sIndex-1 >= 0) {sIndex--;}
-                else{ 
-                    result = true; 
-                    break;
-                }
+        int sPointer = 0;
+        for(int tPointer = 0; tPointer < t.length(); tPointer++){
+            char currChar = t.charAt(tPointer);
+            if(s.charAt(sPointer) == currChar) { 
+                sPointer++;
+
+                // if sIndex (found chars) equal s length, then all of s found in t before t ran out
+                // s.length vs s.length-1 becasue sIndex starts at 0 which technically means it found a char
+                // even though it didnt
+                if(sPointer == s.length()) {return true;}
             }
         }
 
-        return result;
+        return false;
     }
 }
