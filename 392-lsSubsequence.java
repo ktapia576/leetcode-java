5b30524181fa005b8002c9f4419a958ad19c1170
Kevin Tapia <ktapia576@hotmail.com>
2025-03-10 21:58:53 -0400

    Add notes

--- 392-lsSubsequence.java
+++ 392-lsSubsequence.java
@@ -1,3 +1,15 @@
+// Iterate s string for every char and compare to each char in t iteratively
+
+// duplicates can be ignored, since once you find a char you can just proceed to look for next char
+// no matter what is in between the next char subsequence
+
+// I implented the iterative approach in reverse because intially I wanted to add the subsequence
+// into a stack and then compare... but I realized that was not needed since if a char was found
+// and all other subsequennt chars in s were found (before running out of chars in t) then the 
+// subsequence exists
+
+// time: O(n)
+// space: O(1
 class Solution {
     public boolean isSubsequence(String s, String t) {
         boolean result = false;
