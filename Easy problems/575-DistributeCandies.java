c5fdbdfe35689a1cadc24ea9c2430c5c7acb3329
Kevin Tapia <ktapia576@hotmail.com>
2025-03-12 21:22:55 -0400

    Add no hashset approach

--- Easy problems/575-DistributeCandies.java
+++ Easy problems/575-DistributeCandies.java
@@ -29,3 +29,31 @@ class Solution {
         else { return allowedCandy; } 
     }
 }
+
+/* O(n) time and O(1) space apporoach
+
+this gets rid of HashSet usage by creating a seen array and leveraging
+int range contraint
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        boolean[] seen = new boolean[200001]; // leveraging -105 <= candyType[i] <= 105, also defaults as false each index
+        
+        int uniqueCandy = 0, allowedCandy = candyType.length/2;
+
+        for(int i=0; i < candyType.length;i++){
+            int seenIndex = candyType[i] + 100000; // convert negatives to positive number index
+            if(!seen[seenIndex]){
+                seen[seenIndex] = true;
+                uniqueCandy++;
+                if(uniqueCandy > allowedCandy){ break; }
+            }
+        }
+
+        if(allowedCandy >= uniqueCandy) { return uniqueCandy; }
+        else { return allowedCandy; } 
+    }
+}
+
+
+*/
