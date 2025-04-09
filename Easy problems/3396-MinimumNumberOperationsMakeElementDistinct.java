ed27a0f84d569d4233e5d76eba541ae3d9164ef8
ktapia576 <ktapia576@hotmail.com>
2025-04-09 10:33:12 -0400

    adding optimized approach assuming nums[i] < 100

--- Easy problems/3396-MinimumNumberOperationsMakeElementDistinct.java
+++ Easy problems/3396-MinimumNumberOperationsMakeElementDistinct.java
@@ -75,3 +75,18 @@ class Solution {
         return (index+3)/3; // get the ceiling calcuation for the index
     }
 }
+
+// Further optimized solution but using boolean array and ASSUMING nums[i] < 100
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[101];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(seen[nums[i]])
+                return i / 3 + 1;
+            else seen[nums[i]] = true;
+        }
+        return 0;
+    }
+}
