4eb8ab927a5bf2980d08b829c057df2b8df890e8
ktapia576 <ktapia576@hotmail.com>
2025-04-21 19:32:24 -0400

    Add solution to leetcode 1480

---
+++ Easy problems/1480-RunningSumOf1dArray.java
@@ -0,0 +1,11 @@
+// prefixSum problem lol very easy
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+
+        return nums;
+    }
+}
