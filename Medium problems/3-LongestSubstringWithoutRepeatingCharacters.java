0f65e11508d84fe5fb37c89249434b739f16b779
ktapia576 <ktapia576@hotmail.com>
2025-10-10 04:02:43 -0400

    add second solution and add notes to first

--- Medium problems/3-LongestSubstringWithoutRepeatingCharacters.java
+++ Medium problems/3-LongestSubstringWithoutRepeatingCharacters.java
@@ -26,14 +26,15 @@ class Solution {
         HashSet<Character> set = new HashSet<>();
 
         int maxK = 0;
-        int k = 0;
+        int k = 0; // keep track of window size and use that
 
         for(int i = 0; i < s.length(); i++){
-            k++;
+            k++; // always add 1 to k since length of window increased one by going to right once too
             char currChar = s.charAt(i);
 
             while(set.contains(currChar)){
-                char removeChar = s.charAt(i - k + 1);
+                // offset the indexing by 1 since k will always be greater than i, (i makes sure no negative indexing)
+                char removeChar = s.charAt(i - k + 1); 
                 set.remove(removeChar);
                 k--;
             }
@@ -43,5 +44,31 @@ class Solution {
         }
 
         return maxK;
+    }
+}
+
+// SECOND METHOD with using bounds left and right vs using length like first solution
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        int maxLength = 0;
+        int left = 0;
+
+        // right is right window bound
+        // left it left side of sliding window
+        for(int right = 0; right < s.length(); right++){
+            char currChar = s.charAt(right);
+
+            while(set.contains(currChar)){
+                char removeChar = s.charAt(left++);
+                set.remove(removeChar);
+            }
+            set.add(currChar);
+
+            maxLength = Math.max(right - left + 1, maxLength);
+        }
+
+        return maxLength;
     }
 }
